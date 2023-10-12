5b4164bc9b01f76bbd907eec704f5f064a827a43
Valentina De Mite <valesakura97@gmail.com>
2023-10-12 16:30:08 +0200

    added header

--- index.php
+++ index.php
@@ -15,17 +15,26 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
+    <link rel="stylesheet" href="./assets/style.css">
     <title>Spotify</title>
 </head>
 <body>
 <div id="app">
-        <main>
+
+<header> 
+    <nav class="navbar navbar-expand navbar-light bg-dark">
+        <div class="nav navbar-nav">
+            <img src="./assets/img/spotify-logo-png-7053.png" class="w-25 p-3" alt="" srcset="">g
+        </div>
+    </nav>
+</header>
+
+<main>
             <div class="container mt-5">
 
                 <div class="row g-5">
                     <div v-for="song in songs" class="col-4">
                         <div class="card d-flex flex-column justify-content-center align-items-center py-3">
-
                             <div class="d-flex ">
                                 <img class="img-fluid" :src="song.poster" alt="">
                             </div>
@@ -40,6 +49,9 @@
 
             </div>
         </main>
+
+</div>
+        
     </div>
 
 
